1aaf7228930e131220c9cd292aaf07f2f0469cc7
Lucas Silverio <lcs.silva.silverio@gmail.com>
2024-01-30 21:41:04 -0300

    feat: Lesson 35 - Button and Home tests

---
+++ src/components/Button/Buttun.spec.jsx
@@ -0,0 +1,33 @@
+import {fireEvent, render, screen} from '@testing-library/react'
+import {Button} from '.'
+
+describe('Tests to Button component', () => {
+  test('should render the button with the text', () => {
+    render(<Button text="Load more"/>)
+    expect.assertions(1)
+
+    const button = screen.getByRole('button', {name: /load more/i })
+    expect(button).toBeInTheDocument()
+  })
+  test('should call function on button click', () => {
+    const handleClick = jest.fn()
+    render(<Button text="Load more" onClick={handleClick}/>)
+    const button = screen.getByRole('button', {name: /load more/i })
+    fireEvent.click(button)
+
+    expect(handleClick).toHaveBeenCalled()
+  })
+  test('should be disabled when disabled is true', () => {
+    render(<Button text="Load more" disabled={true}/>)
+
+    const button = screen.getByRole('button', {name: /load more/i })
+    expect(button).toBeDisabled()
+  })
+
+  test('should be enable when enable is true', () => {
+    render(<Button text="Load more" disabled={false}/>)
+
+    const button = screen.getByRole('button', {name: /load more/i })
+    expect(button).not.toBeDisabled()
+  })
+})
